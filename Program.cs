a8057f21d2a2910f653cc153cb1117bae4cbc207
D_M <deadlycookye@gmail.com>
2023-05-25 19:28:39 +0300

    separate failed attempt

--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-// int[] arr = InsertionSort.insertSort_Decreasing();
-//         String output = string.Join(", ", arr);
-//         Console.WriteLine(output);
-Console.WriteLine(LinearSearch.basicLinearSearch(67));
---
+++ csharp_imp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int[] arr = InsertionSort.insertSort_Decreasing();
+//         String output = string.Join(", ", arr);
+//         Console.WriteLine(output);
+Console.WriteLine(LinearSearch.basicLinearSearch(67));
